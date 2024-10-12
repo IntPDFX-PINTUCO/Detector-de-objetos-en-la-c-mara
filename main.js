7eab2b2fc21f9966afe3efa968c6c7b9d16fc84c
IntPDFX-PINTUCO <127631195+IntPDFX-PINTUCO@users.noreply.github.com>
2024-10-12 16:09:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 foto = ""
 lista = []
 function setup() {
-    canvas = createCanvas(foto.width, foto.height)
+    canvas = createCanvas(0, 0)
     background("blue")
     ia = ml5.objectDetector("cocossd", ia_cargada)
 }
-function preload() {
-    foto = loadImage("dog_cat.jpeg")
-}
 function draw() {
-    image(foto, 0, 0, width, height)
-    if(lista.length>0){
+    if (foto != "") {
+        image(foto, 0, 0, width, height)
+    }
+    if (lista.length > 0) {
         textSize(17)
         lista.forEach(element => {
             noFill()
             stroke("#0099f9")
             strokeWeight(5)
             rect(element.x, element.y, element.width, element.height)
-                        strokeWeight(3)
+            strokeWeight(3)
             fetch("https://api.mymemory.translated.net/get?q=" + element.label + "&langpair=en|es")
-            .then(response => response.json())
-            .then(data => {
-                traduccion = data.responseData.translatedText; console.log(traduccion)
-                fill("black")
-                text(traduccion, element.x, element.y)
-            });
+                .then(response => response.json())
+                .then(data => {
+                    traduccion = data.responseData.translatedText; console.log(traduccion)
+                    fill("black")
+                    text(traduccion, element.x, element.y)
+                });
         });
         noLoop()
     }
@@ -46,11 +45,11 @@ function detectar() {
     ia.detect(foto, resultado)
 }
 function c() {
-    imagen=document.getElementById("subir").files[0]
-    if(imagen){
-        foto = loadImage(URL.createObjectURL(imagen))
-        foto.resize(width,height)
-        lista=[]
+    imagen = document.getElementById("subir").files[0]
+    if (imagen) {
+        archimg = URL.createObjectURL(imagen)
+        foto = loadImage( archimg, () => { resizeCanvas(foto.width, foto.height); image(foto, 0, 0) })
+        lista = []
         detectar()
         loop()
     }
